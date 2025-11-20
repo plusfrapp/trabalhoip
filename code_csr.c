016a0308850219babedda04b1eb1951f25563c56
cesarolf <cesar.oliveiralf34@gmail.com>
2025-11-19 23:08:43 -0300

    Atualiza code_csr.c

--- code_csr.c
+++ code_csr.c
@@ -1,8 +1,15 @@
+void multiplicacao( int entrada1, int entrada2, int * saida);
+void xor_bits( int entrada1, int entrada2, int tam, int * saida);
+void maximo( int entrada1, int entrada2, int * saida);
+void menor_igual( int entrada1, int entrada2, int * saida);
+
+
 void multiplicacao( int entrada1, int entrada2, int * saida){
 
     * saida = ( entrada1) * ( entrada2);
 }
 
+/*
 void xor_bits( int * entrada1, int * entrada2, int tam, int * saida){
 
     int i;
@@ -11,6 +18,12 @@ void xor_bits( int * entrada1, int * entrada2, int tam, int * saida){
         if ( * (entrada1 + i) == * (entrada2 + i)) * (saida + i) = 0;
         else if ( * (entrada1 + i) != * (entrada2 + i)) * (saida + i) = 1;
     } 
+}
+*/
+
+void xor_bits( int entrada1, int entrada2, int tam, int * saida){
+
+    * saida = entrada1 ^ entrada2;
 }
 
 void maximo( int entrada1, int entrada2, int * saida){
