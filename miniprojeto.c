a6c0ea55cad3143951ba5f0a1bbc1e7d33f9e0e5
Gabriel Vidal <gabrielnext287@gmail.com>
2025-11-28 22:21:56 -0300

    Subindo alteracoes

--- miniprojeto.c
+++ miniprojeto.c
@@ -1,48 +1,79 @@
 #include <stdio.h>
+#include <string.h>
 //1
-void sub(int *entrada1, int *entrada2, int *saida); // Função 2
-void multiplicacao( int entrada1, int entrada2, int * saida); //3
-void div(int a, int b, double * c); //4
-void Resto(int entrada1, int entrada2, int *resposta); //5
-void potencia(int entrada1, int entrada2, int *resposta); //6
-void fatoracao(int * entrada1, int * saida); //7
+void sub(int entrada1, int entrada2, int * resposta); // Função 2
+void multiplicacao( int entrada1, int entrada2, int * resposta); //3
+void div(int entrada1, int entrada2, double * resposta); //4
+void Resto(int entrada1, int entrada2, int * resposta); //5
+void potencia(int entrada1, int entrada2, int * resposta); //6
+void fatoracao(int entrada1, int * resposta); //7
 //8
-void or_bit_a_bit(int *entrada1, int *entrada2, int *saida); // Função 9
-void xor_bits( int entrada1, int entrada2, int tam, int * saida); //10
-void shiftDir(int *a, int b); //11
-void Shift_A_Esquerda(int entrada1, int entrada2, int *resposta); //12
+void or_bit_a_bit(int entrada1, int entrada2, int * resposta); // Função 9
+void xor_bits( int entrada1, int entrada2, int tam, int * resposta); //10
+void shiftDir(int entrada1, int entrada2, int *resposta); //11
+void Shift_A_Esquerda(int entrada1, int entrada2, int * resposta); //12
 //13
-void s_intervalo( int * entrada1, int * entrada2, long long int * saida); //14
+void s_intervalo( int entrada1, int entrada2, long long int * resposta); //14
 //15
-void min(int *entrada1, int *entrada2, int *saida); // Função 16
-void maximo( int entrada1, int entrada2, int * saida); //17
-void abs(int *a); //18
+void min(int entrada1, int entrada2, int * resposta); // Função 16
+void maximo( int entrada1, int entrada2, int * resposta); //17
+void absoluto(int entrada1, int *resposta); //18
 void Igualdade(int entrada1, int entrada2, int *resposta); //19
 //20
-void maiorq( int * entrada1, int * entrada2, int * saida); //21
+void maiorq( int entrada1, int entrada2, int * resposta); //21
 //22
-void maior_igual(int *entrada1, int *entrada2, int *saida); //Função 23
-void menor_igual( int entrada1, int entrada2, int * saida); //24
-void and(int a, int b, char * str); //25
-void NAND(int entrada1, int entrada2, int *resposta); //26
+void maior_igual(int entrada1, int entrada2, int * resposta); //Função 23
+void menor_igual( int entrada1, int entrada2, int * resposta); //24
+void and(int entrada1, int entrada2, char * resposta); //25
+void NAND(int entrada1, int entrada2, int * resposta); //26
 //27
-void nor( int * entrada1, int * entrada2, int * saida); //28
+void nor( int entrada1, int entrada2, int * resposta); //28
 
 int main(){
+
+    printf("Funcoes disponiveis:\n");
+    printf("1) Soma (+)\n");
+    printf("2) Subtracao (-)\n");
+    printf("3) Multiplicacao (*)\n");
+    printf("4) Divisao (/)\n");
+    printf("5) Resto (%%)\n");
+    printf("6) Potencia (~)\n");
+    printf("7) Fatoracao (!)\n");
+    printf("8) AND bit a bit (&)\n");
+    printf("9) OR bit a bit (|)\n");
+    printf("10) XOR bit a bit (^)\n");
+    printf("11) Shift a direita (>>)\n");
+    printf("12) Shift a esquerda (<<)\n");
+    printf("13) Media de dois numeros (M)\n");
+    printf("14) Somatorio de um intervalo (S)\n");
+    printf("15) Produtorio de um intervalo (P)\n");
+    printf("16) Minimo entre dois numeros (min)\n");
+    printf("17) Maximo entre dois numeros (max)\n");
+    printf("18) Valor absoluto de um numero (abs)\n");
+    printf("19) Igualdade (==)\n");
+    printf("20) Diferenca (!=)\n");
+    printf("21) Maior que (>)\n");
+    printf("22) Menor que (<)\n");
+    printf("23) Maior ou igual (>=)\n");
+    printf("24) Menor ou igual (<=)\n");
+    printf("25) AND (&&)\n");
+    printf("26) NAND (!&)\n");
+    printf("27) OR (||)\n");
+    printf("28) NOR (!|)\n");
 
     return 0;
 }
 
-void sub(int *entrada1, int *entrada2, int *saida){ //2
-    *saida = *entrada1 - *entrada2;
+void sub(int entrada1, int entrada2, int * resposta){ //2
+    *resposta = entrada1 - entrada2;
 }
-void multiplicacao( int entrada1, int entrada2, int * saida){ //3
-    * saida = ( entrada1) * ( entrada2);
+void multiplicacao( int entrada1, int entrada2, int * resposta){ //3
+    * resposta = ( entrada1) * ( entrada2);
 }
-void div(int a, int b, double * c){ //4
-    *c = a/(double)b;
+void div(int entrada1, int entrada2, double * resposta){ //4
+    *resposta = entrada1/(double)entrada2;
 }
-void Resto(int entrada1, int entrada2, int *resposta){ //5
+void Resto(int entrada1, int entrada2, int * resposta){ //5
     *resposta = entrada1%entrada2;
 }
 void potencia(int entrada1, int entrada2, int *resposta){ //6
@@ -55,81 +86,83 @@ void potencia(int entrada1, int entrada2, int *resposta){ //6
         }
     }
 }
-void fatoracao(int * entrada1, int * saida){ //7
+void fatoracao(int entrada1, int * resposta){ //7
     
     int i;
-    *saida=1;
+    *resposta=1;
     
-    for(i=1;i<*entrada1;i++) *saida*=i;
+    for(i=1;i<entrada1;i++) *resposta*=i;
     
 }
-void or_bit_a_bit(int *entrada1, int *entrada2, int *saida){ //9
-    *saida = *entrada1 | *entrada2;
+void or_bit_a_bit(int entrada1, int entrada2, int * resposta){ //9
+    *resposta = entrada1 | entrada2;
 }
-void xor_bits( int entrada1, int entrada2, int tam, int * saida){ //10
-    * saida = entrada1 ^ entrada2;
+void xor_bits( int entrada1, int entrada2, int tam, int * resposta){ //10
+    * resposta = entrada1 ^ entrada2;
 }
-void shiftDir(int *a, int b) { //11
-    *a /= pow(2, b);
+void shiftDir(int entrada1, int entrada2, int * resposta) { //11
+    int pow;
+    potencia(2, entrada2, &pow);
+    *resposta = entrada1/pow;
 }
-void Shift_A_Esquerda(int entrada1, int entrada2, int *resposta){ //12
+void Shift_A_Esquerda(int entrada1, int entrada2, int * resposta){ //12
     int pow;
     potencia(2,entrada2,&pow);
     *resposta = entrada1*pow;
 }
-void s_intervalo( int * entrada1, int * entrada2, long long int * saida){ //14
+void s_intervalo( int entrada1, int entrada2, long long int * resposta){ //14
 
-    int i,n=*entrada2-*entrada1+1,temp;
+    int i,n=entrada2-entrada1+1,temp;
     
-    if(*entrada1>*entrada2){
-        temp=*entrada1;
-        *entrada1=*entrada2;
-        *entrada2=temp;
+    if(entrada1>entrada2){
+        temp=entrada1;
+        entrada1=entrada2;
+        entrada2=temp;
     }
 }
-void min(int *entrada1, int *entrada2, int *saida){ //16
-    if(*entrada1 < *entrada2) *saida = *entrada1;
-    else *saida = *entrada2;
+void min(int entrada1, int entrada2, int * resposta){ //16
+    if(entrada1 < entrada2) *resposta = entrada1;
+    else *resposta = entrada2;
 }
-void maximo( int entrada1, int entrada2, int * saida){ //17
-    if ( entrada1 > entrada2 ) * saida = entrada1;
-    else * saida = entrada2;
+void maximo( int entrada1, int entrada2, int * resposta){ //17
+    if ( entrada1 > entrada2 ) * resposta = entrada1;
+    else * resposta = entrada2;
 }
-void abs(int *a){ //18
-    if(*a < 0) *a *= -1;
+void absoluto(int entrada1, int *resposta){ //18
+    if(entrada1 < 0) *resposta = -1*entrada1;
 }
-void Igualdade(int entrada1, int entrada2, int *resposta){ //19
+void Igualdade(int entrada1, int entrada2, int * resposta){ //19
     if(entrada1==entrada2) *resposta = 1;
     else *resposta = 0;
 }
-void maiorq( int * entrada1, int * entrada2, int * saida){ //21
+void maiorq( int entrada1, int entrada2, int * resposta){ //21
 
-    if(*entrada1>*entrada2)*saida=1;
-    else *saida=0;
+    if(entrada1>entrada2)*resposta=1;
+    else *resposta=0;
 
 }
-void maior_igual(int *entrada1, int *entrada2, int *saida){ //23
-    if(*entrada1 >= *entrada2) *saida = 1;
-    else *saida = 0;
+void maior_igual(int entrada1, int entrada2, int * resposta){ //23
+    if(entrada1 >= entrada2) *resposta = 1;
+    else *resposta = 0;
 }
-void menor_igual( int entrada1, int entrada2, int * saida){ //24
-    if ( entrada1 <= entrada2 ) * saida = 1;
-    else * saida = 0;
+void menor_igual( int entrada1, int entrada2, int * resposta){ //24
+    if ( entrada1 <= entrada2 ) * resposta = 1;
+    else * resposta = 0;
 }
-void and(int a, int b, char * str){ //25
+void and(int entrada1, int entrada2, char * str){ //25
     int i;
     char sim[4] = {"SIM"}, nao[4] = {"NAO"};
 
-    if((a==1 && b==0) || (a==0 && b==1)) strcpy(str, nao);
-    if((a==1 && b==1) || (a==0 && b==0)) strcpy(str, sim);
+    if((entrada1==1 && entrada2==0) || (entrada1==0 && entrada2==1)) strcpy(str, nao);
+    if((entrada1==1 && entrada2==1) || (entrada1==0 && entrada2==0)) strcpy(str, sim);
 }
-void NAND(int entrada1, int entrada2, int *resposta){ //26
+void NAND(int entrada1, int entrada2, int * resposta){ //26
     if(entrada1==1 && entrada2==1) *resposta = 0;
     else *resposta = 1;
 }
-void nor( int * entrada1, int * entrada2, int * saida){ //28
+void nor( int entrada1, int entrada2, int * resposta){ //28
 
-    if(*entrada1==0&&*entrada2==0)*saida=1;
-    else *saida = 0;
+    if(entrada1==0&&entrada2==0)*resposta=1;
+    else *resposta = 0;
 
 }
